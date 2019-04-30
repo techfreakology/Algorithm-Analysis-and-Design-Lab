962e0066bafdf3c562bfe2476a349a59a602e126
vivekcrux <techfreakology@gmail.com>
2019-04-30 20:44:24 +0530

    added quick sort and merge sort

---
+++ merge_sort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int a[],int s,int e)
+{
+	int mid = (s+e)/2;
+	int i,j,k,temp[e-s];
+	i = s,j = mid,k = 0;
+	while(i<mid && j<e)
+	{
+		if(a[i]<=a[j])
+		{
+			temp[k] = a[i];
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			j++;
+		}
+		k++;
+	}
+	while(i<mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+	while(j<e)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+	for(i=s;i<e;i++)
+	{
+		a[i] = temp[i-s];
+	}
+}
+
+void merge_sort(int a[],int s,int e)
+{
+	if(e-s<=1)	return;
+	int mid = (s+e)/2;
+	merge_sort(a,s,mid);
+	merge_sort(a,mid,e);
+	merge(a,s,e);
+}
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {6,1,7,12,5,1,67,1,3,41,2,65};
+	merge_sort(a,0,sizeof(a)/sizeof(int));
+	for(auto it: a)
+		cout<<it<<" ";
+	return 0;
+}
---
+++ quick_sort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(int a[],int s,int e)
+{
+	int i,j;
+	int p = e;
+	j = s-1;
+	for(i=s;i<e;i++)
+	{
+		if(a[i]<=a[e])
+		{
+			swap(a[i],a[j+1]);
+			j++;
+		}
+	}
+	swap(a[j+1],a[e]);
+	return j+1;
+}
+
+void quick_sort(int a[],int s,int e)
+{
+	if(s>=e)	return;
+	int p = partition(a,s,e-1);
+	quick_sort(a,s,p);
+	quick_sort(a,p+1,e);
+}
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {6,1,7,12,5,1,67,1,3,41,2,65};
+	quick_sort(a,0,sizeof(a)/sizeof(int));
+	for(auto it: a)
+		cout<<it<<" ";
+	return 0;
+}
